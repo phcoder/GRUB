fd0db7431461b8280a4ff57eee7239cc9692ab1d
Vladimir Serbinenko <phcoder@gmail.com>
2024-04-04 22:39:28 +0300

    1 more func

--- grub-core/lib/libgcrypt_wrap/mem.c
+++ grub-core/lib/libgcrypt_wrap/mem.c
@@ -70,6 +70,15 @@ gcry_xcalloc_secure (size_t n, size_t m)
 }
 
 void *
+_gcry_calloc_secure (size_t n, size_t m)
+{
+  size_t sz;
+  if (grub_mul (n, m, &sz))
+    return NULL;
+  return grub_zalloc (sz);
+}
+
+void *
 gcry_xmalloc (size_t n)
 {
   void *ret;
@@ -102,6 +111,24 @@ _gcry_check_heap (const void *a __attribute__ ((unused)))
 }
 
 void _gcry_log_printf (const char *fmt, ...)
+{
+  va_list args;
+  const char *debug = grub_env_get ("debug");
+
+  if (! debug)
+    return;
+
+  if (grub_strword (debug, "all") || grub_strword (debug, "gcrypt"))
+    {
+      grub_printf ("gcrypt: ");
+      va_start (args, fmt);
+      grub_vprintf (fmt, args);
+      va_end (args);
+      grub_refresh ();
+    }
+}
+
+void _gcry_log_debug (const char *fmt, ...)
 {
   va_list args;
   const char *debug = grub_env_get ("debug");
