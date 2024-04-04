b5368d05d66aa4e61f352cf859000f34bb134845
Vladimir Serbinenko <phcoder@gmail.com>
2024-04-04 17:40:02 +0300

    Mark vdev_zaps_v2 and head_errlog as supported

--- grub-core/fs/zfs/zfs.c
+++ grub-core/fs/zfs/zfs.c
@@ -293,6 +293,8 @@ static const char *spa_feature_names[] = {
   "com.delphix:embedded_data",
   "com.delphix:extensible_dataset",
   "org.open-zfs:large_blocks",
+  "com.klarasystems:vdev_zaps_v2",
+  "com.delphix:head_errlog",
   "org.freebsd:zstd_compress",
   "com.datto:encryption",
   NULL
