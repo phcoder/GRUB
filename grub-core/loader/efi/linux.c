4bce59aebfbe0a0eeca60f36b68355492ddf9ebc
Vladimir Serbinenko <phcoder@gmail.com>
2024-09-10 02:13:07 +0300

    efi/linux: Silence warning

--- grub-core/loader/efi/linux.c
+++ grub-core/loader/efi/linux.c
@@ -55,6 +55,7 @@ static bool initrd_use_loadfile2 = false;
 static grub_guid_t load_file2_guid = GRUB_EFI_LOAD_FILE2_PROTOCOL_GUID;
 static grub_guid_t device_path_guid = GRUB_EFI_DEVICE_PATH_GUID;
 
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 static initrd_media_device_path_t initrd_lf2_device_path = {
   {
     {
